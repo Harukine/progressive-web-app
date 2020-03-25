76e6ce28d2648a58e75253ff9731f9aed36c5d6d
Harukine <jacobhtnguyen@gmail.com>
2020-03-25 03:31:35 -0400

    imported Material Toolbar and Icon Modules

--- CH1/lovely-offline/src/app/app.module.ts
+++ CH1/lovely-offline/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,6 +14,8 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
     AppRoutingModule
   ],
   providers: [],
