0cea576142b1de88c160a76d5d98324c44c62df3
Harukine <jacobhtnguyen@gmail.com>
2020-03-28 03:52:05 -0400

    implemented auth service

--- lovely-offline/src/app/modules/core/auth.service.ts
+++ lovely-offline/src/app/modules/core/auth.service.ts
@@ -1,9 +1,79 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {BehaviorSubject, Subject} from 'rxjs';
+import {AngularFireAuth} from '@angular/fire/auth';
+import {first, tap} from 'rxjs/operators';
+import {User} from 'firebase';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  // expose all data
+  public authErrorMessages$ = new Subject<string>();
+  public isLoading$ = new BehaviorSubject<boolean>(true);
+  public user$ = new Subject<User>();
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth) {
+    this.isLoggedIn().subscribe();
+  }
+
+  private isLoggedIn() {
+    return this.afAuth.authState.pipe(
+      first(),
+      tap(user => {
+        this.isLoading$.next(false);
+        if (user) {
+          // const { email, uid } = user;
+          this.user$.next(user);
+        }
+      })
+    );
+  }
+
+  public signUpFirebase({email, password}) {
+    this.isLoading$.next(true);
+    this.handleErrorOrSuccess(() => {
+      return this.afAuth.auth.createUserWithEmailAndPassword(email, password);
+    });
+  }
+
+  public loginFirebase({email, password}) {
+    this.isLoading$.next(true);
+    this.handleErrorOrSuccess(() => {
+      return this.afAuth.auth.signInWithEmailAndPassword(email, password);
+    });
+  }
+
+  public logOutFirebase() {
+    this.isLoading$.next(true);
+    this.afAuth.auth
+      .signOut()
+      .then(() => {
+        this.isLoading$.next(false);
+        this.user$.next(null);
+      })
+      .catch(e => {
+        console.error(e);
+        this.isLoading$.next(false);
+        this.authErrorMessages$.next('Something is wrong when signing out!');
+      });
+  }
+
+  private handleErrorOrSuccess(cb: () => Promise<firebase.auth.UserCredential>) {
+    cb()
+      .then(data => this.authenticateUser(data))
+      .catch(e => this.handleSignUpLoginError(e));
+  }
+
+  private authenticateUser(UserCredential) {
+    // const {user: { email, uid }} = UserCredential;
+    this.isLoading$.next(false);
+    this.user$.next(UserCredential.user);
+  }
+
+  private handleSignUpLoginError(error: { code: string; message: string }) {
+    this.isLoading$.next(false);
+    const errorMessage = error.message;
+    this.authErrorMessages$.next(errorMessage);
+  }
 }
