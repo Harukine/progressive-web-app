048961e8b43cf4a7af92213f2d3d832bd39b69c7
Harukine <jacobhtnguyen@gmail.com>
2020-03-28 02:25:02 -0400

    implemented user module routing

--- lovely-offline/src/app/modules/user/user-routing.module.ts
+++ lovely-offline/src/app/modules/user/user-routing.module.ts
@@ -1,11 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {UserContainerComponent} from './user-container.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: UserContainerComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class UserRoutingModule { }
+export class UserRoutingModule {
+}
