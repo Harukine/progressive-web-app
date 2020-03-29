ab77851a663d3f7bb12cfea6b38232ebd7c48d0f
Harukine <jacobhtnguyen@gmail.com>
2020-03-29 05:57:56 -0400

    updated auth service

--- lovely-offline/src/app/modules/core/auth.service.ts
+++ lovely-offline/src/app/modules/core/auth.service.ts
@@ -1,33 +1,53 @@
 import {Injectable} from '@angular/core';
-import {BehaviorSubject, Subject} from 'rxjs';
+import {BehaviorSubject, Observable} from 'rxjs';
 import {AngularFireAuth} from '@angular/fire/auth';
-import {first, tap} from 'rxjs/operators';
-import {User} from 'firebase';
+import {map, tap} from 'rxjs/operators';
+import * as firebase from 'firebase';
+
+interface User {
+  uid: string;
+  email: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   // expose all data
-  public authErrorMessages$ = new Subject<string>();
+  public authErrorMessages$ = new BehaviorSubject<string>(null);
   public isLoading$ = new BehaviorSubject<boolean>(true);
-  public user$ = new Subject<User>();
+  public user$ = new BehaviorSubject<User>(null);
+
+  private authState = null;
 
   constructor(private afAuth: AngularFireAuth) {
-    this.isLoggedIn().subscribe();
+    this.isLoggedIn().subscribe(user => (this.authState = user));
   }
 
-  private isLoggedIn() {
+  get authenticated(): boolean {
+    return this.authState !== null;
+  }
+
+  get id(): string {
+    return this.authenticated ? this.authState.uid : '';
+  }
+
+  private isLoggedIn(): Observable<User | null> {
     return this.afAuth.authState.pipe(
-      first(),
-      tap(user => {
-        this.isLoading$.next(false);
+      map(user => {
         if (user) {
-          // const { email, uid } = user;
-          this.user$.next(user);
+          const {email, uid} = user;
+          this.user$.next({email, uid});
+          return {email, uid};
         }
-      })
+        return null;
+      }),
+      tap(() => this.isLoading$.next(false))
     );
+  }
+
+  public getCurrentUserUid(): string {
+    return this.afAuth.auth.currentUser.uid;
   }
 
   public signUpFirebase({email, password}) {
@@ -46,29 +66,23 @@ export class AuthService {
 
   public logOutFirebase() {
     this.isLoading$.next(true);
-    this.afAuth.auth
-      .signOut()
-      .then(() => {
-        this.isLoading$.next(false);
-        this.user$.next(null);
-      })
-      .catch(e => {
-        console.error(e);
-        this.isLoading$.next(false);
-        this.authErrorMessages$.next('Something is wrong when signing out!');
-      });
+    return this.afAuth.auth.signOut();
   }
 
-  private handleErrorOrSuccess(cb: () => Promise<firebase.auth.UserCredential>) {
+  private handleErrorOrSuccess(
+    cb: () => Promise<firebase.auth.UserCredential>
+  ) {
     cb()
       .then(data => this.authenticateUser(data))
       .catch(e => this.handleSignUpLoginError(e));
   }
 
   private authenticateUser(UserCredential) {
-    // const {user: { email, uid }} = UserCredential;
+    const {
+      user: {email, uid}
+    } = UserCredential;
     this.isLoading$.next(false);
-    this.user$.next(UserCredential.user);
+    this.user$.next({email, uid});
   }
 
   private handleSignUpLoginError(error: { code: string; message: string }) {
