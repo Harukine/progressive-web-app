236d581c83ddb1b7844d5a3d656322d3e0041059
Harukine <jacobhtnguyen@gmail.com>
2020-03-29 05:02:32 -0400

    imported components

--- lovely-offline/src/app/modules/notes/notes.module.ts
+++ lovely-offline/src/app/modules/notes/notes.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { NotesRoutingModule } from './notes-routing.module';
+import { NotesListComponent } from './notes-list/notes-list.component';
+import { NotesAddComponent } from './notes-add/notes-add.component';
+import { NoteDetailsComponent } from './note-details/note-details.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [NotesListComponent, NotesAddComponent, NoteDetailsComponent],
   imports: [
     CommonModule,
     NotesRoutingModule
