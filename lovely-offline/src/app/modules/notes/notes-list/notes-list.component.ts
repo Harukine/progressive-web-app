70f07297e7f60d4d02d6be27444d7f58e28d8100
Harukine <jacobhtnguyen@gmail.com>
2020-03-31 06:10:10 -0400

    implemented notes-list component

--- lovely-offline/src/app/modules/notes/notes-list/notes-list.component.ts
+++ lovely-offline/src/app/modules/notes/notes-list/notes-list.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable} from 'rxjs';
+import {DataService} from '../../core/data.service';
+
+interface Note {
+  id: string;
+  title: string;
+  content: string;
+}
 
 @Component({
   selector: 'app-notes-list',
   templateUrl: './notes-list.component.html',
   styleUrls: ['./notes-list.component.scss']
 })
+
 export class NotesListComponent implements OnInit {
+  notes$: Observable<Note[]>;
+  isDbLoading$;
 
-  constructor() { }
+  constructor(private db: DataService) {
+  }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.notes$ = this.db.getNotes();
+    this.isDbLoading$ = this.db.isLoading$;
   }
 
 }
