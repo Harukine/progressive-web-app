f9aafcbc420dd2728954fd65bdaa45a2d86bd7d7
Harukine <jacobhtnguyen@gmail.com>
2020-03-31 06:00:04 -0400

    implemented auth guard

--- lovely-offline/src/app/modules/notes/auth.guard.ts
+++ lovely-offline/src/app/modules/notes/auth.guard.ts
@@ -1,14 +1,20 @@
 import {Injectable} from '@angular/core';
-import {CanLoad, Route, UrlSegment} from '@angular/router';
-import {Observable} from 'rxjs';
+import {CanLoad, Router} from '@angular/router';
+import {Observable, of} from 'rxjs';
+import {AuthService} from '../core/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanLoad {
-  canLoad(
-    route: Route,
-    segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
+  constructor(private auth: AuthService, private router: Router) {
+  }
+
+  canLoad(): Observable<boolean> {
+    if (!this.auth.authenticated) {
+      this.router.navigate(['/user']);
+      return of(false);
+    }
+    return of(true);
   }
 }
